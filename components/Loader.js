9882715304794e1da74d65eaba474d27edc87b00
Shivnath Kisku <124551959+Shivkisku@users.noreply.github.com>
2024-09-18 11:16:19 +0530

    Add files via upload

---
+++ components/Loader.js
@@ -0,0 +1,19 @@
+// components/Loader.js
+import React from 'react';
+import styles from './Loader.module.css';
+
+const Loader = () => {
+  return (
+    <div className={styles.loader}>
+      <div className={styles.loaderSquare}></div>
+      <div className={styles.loaderSquare}></div>
+      <div className={styles.loaderSquare}></div>
+      <div className={styles.loaderSquare}></div>
+      <div className={styles.loaderSquare}></div>
+      <div className={styles.loaderSquare}></div>
+      <div className={styles.loaderSquare}></div>
+    </div>
+  );
+};
+
+export default Loader;
